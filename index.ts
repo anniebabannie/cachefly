d1b7baef8b27051630410d1bddab62b2117ffdf3
Michael Dwan <m@dwan.io>
2020-02-18 12:46:21 -0600

    port path handling & origin fetch from edge app

--- index.ts
+++ index.ts
@@ -26,6 +26,19 @@ function intOrUndefined(raw: string | null | undefined): number | undefined{
   }
   return undefined;
 }
+
+function fetchOrigial(url: URL): Promise<http.IncomingMessage> {
+  return new Promise((resolve, reject) => {
+    try {
+      http.get(url, (response) => {
+        resolve(response)
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
 const filters = new Map<string,FilterFunction> ([
   ["auto_fix", (img, value) => value === "true" ? img.out("-contrast-stretch","2%", "1%") : img],
   ["flip", (img, value) =>value === "true" ? img.flip() : img],
@@ -45,9 +58,17 @@ const filters = new Map<string,FilterFunction> ([
   }]
 ]);
 
+function headerOrDefault(req: http.IncomingMessage, name: string, defaultVal : string): string {
+  const val = req.headers[name.toLowerCase()]
+  if (val) {
+    return val as string
+  }
+  return defaultVal
+}
+
 const authToken = process.env.AUTH_TOKEN;
 // fly deploy:image registry-1.docker.io/nginxdemos/hello:latest
-const server = http.createServer((req, resp) =>{
+const server = http.createServer(async (req, resp) =>{
   console.log([req.httpVersion, req.socket.remoteAddress, req.url, req.headers["user-agent"]].join(" "))
 
   if((req.method === "HEAD" || req.method === "GET") && req.url === "/__status"){
@@ -55,27 +76,63 @@ const server = http.createServer((req, resp) =>{
     resp.end("ok")
     return
   }
-  if(authToken){
-    const auth = req.headers['authorization'] || "";
-    if(auth !== `Bearer ${authToken}`){
-      console.error("auth token mismatch:", auth)
-      resp.writeHead(403);
-      resp.end("no yuo")
-      return;
-    }
-  }
+
   const responseHeaders: http.OutgoingHttpHeaders = {}
   const url = new URL(req.url, "http://magick");
-  if(req.method !== "POST"){
-    resp.writeHead(405);
-    resp.end("")
-    return;
-  }
-  const origin = url.searchParams.get("origin") || url.pathname;
-  url.searchParams.delete("origin");
 
-  const etag = [origin, url.search].map(md5).join("/");
-  const img = gm(req);
+  const originBase = headerOrDefault(req, "Image-Origin", "http://dealercarsearch-sf.static-ord.sctgos.com/");
+  const origin = new URL(url.pathname.substr(1), originBase);
+  const accept = headerOrDefault(req, "accept", "");
+  if (url.search.length < 1) {
+    url.search = headerOrDefault(req, "Image-Operation", "");
+  }
+  
+  let originResp: http.IncomingMessage
+
+  try {
+    originResp = await fetchOrigial(origin)
+    if (originResp.statusCode != 200) {
+      resp.statusCode = originResp.statusCode
+      originResp.pipe(resp)
+      return
+    }
+  } catch (err) {
+    console.error("origin error", err)
+    resp.statusCode = 500
+    resp.end("Error fetching from origin")
+    return
+  }
+  
+  const contentType = headerOrDefault(originResp, "content-type", "")
+
+  if (!contentType.includes("image/")) {
+    resp.statusCode = 400
+    resp.end("unexpected content type: " + contentType)
+    return
+  }
+  
+  let fmt = url.searchParams.get("format")
+  if (fmt === "auto") {
+    if (accept.includes("/webp")) {
+      url.searchParams.set("format", "webb");
+    } else {
+      url.searchParams.delete("format")
+    }
+  }
+
+  if (url.search.length == 0) {
+    for (const [name, val] of Object.entries(originResp.headers)) {
+      resp.setHeader(name, val)
+    }
+    resp.setHeader("timing-allow-origin", "*")
+    originResp.pipe(resp)
+    return
+  }
+
+  // original magick server...
+
+  const etag = [origin.href, url.search].map(md5).join("/");
+  const img = gm(originResp);
   //@ts-ignore
   img._options.imageMagick = true;
 
@@ -95,18 +152,20 @@ const server = http.createServer((req, resp) =>{
   });
 
   img.toBuffer((err, buf) => {
-    if(err){
+    if (err){
       console.trace("error:", err.message);
       resp.writeHead(500)
       //@ts-ignore
-      resp.end(err.toString());
+      resp.end("error processing image");
       return;
     }
     dataOut = buf.length;
-    resp.writeHead(200,Object.assign({
-      "content-type": req.headers["content-type"],
+    resp.writeHead(200, Object.assign({
+      "timing-allow-origin": "*",
+      "content-type": contentType,
       "content-length": buf.length,
-      etag: etag
+      etag: etag,
+      "last-modified": originResp.headers["last-modified"],
     }, responseHeaders));
     resp.end(buf);
     console.log(`${origin}${url.search}, ${dataIn / 1024}kB input, ${dataOut / 1024}kB output, ${new Date().getTime() - startTime}ms`);
